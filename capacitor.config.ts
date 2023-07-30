92ef9eac32e8fdf598b27ecc6fd308008ab51c08
Aaron Saunders <aaronksaunders@gmail.com>
2023-07-30 00:55:52 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vuefire-app-ionic7',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
